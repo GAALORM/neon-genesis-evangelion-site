a0fdd89b2457507c6b6f51899f19ff6e4a8bd2b1
GAALORM <57526859+GAALORM@users.noreply.github.com>
2025-03-25 00:26:25 +0100

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    if (window.location.pathname.endsWith(".html")) {
+    if (window.location.pathname.endsWith(".html") && window.location.search === "") {
         let newUrl = window.location.pathname.replace(".html", "");
         window.history.replaceState(null, "", newUrl);
     }
